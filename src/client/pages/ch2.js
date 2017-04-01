c4d5c4480a9743f2eda37c213d947409c83299bc
JackChan <jack5241027@gmail.com>
2017-04-01 19:03:13 +0800

    Fix: height not overlay whole issue

---
+++ src/client/pages/ch2.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+
+class Ch2 extends React.Component {
+    render() {
+        return (
+            <div className='page'>
+                <Header />
+                <main className='page__container page-ch2'>
+                </main>
+            </div>
+        )
+    }
+}
+
+export default Ch2
--- src/client/routes.js
+++ src/client/routes.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import Index from './pages/index'
+import Ch2 from './pages/Ch2'
 
 export default (
     <Route path='/'>
         <IndexRoute component={Index} />
+        <Route path='ch2' component={Ch2} />
     </Route>
 )
